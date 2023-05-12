5ce0be3569d4c05538ba5903eeadb64ea338a136
nananapo <kanapodream@gmail.com>
2023-05-12 12:53:08 +0900

    .

--- test/sample.pl
+++ test/sample.pl
@@ -11,7 +11,12 @@ b(0,1).
 b(1,1).
 
 even(0).
+even(0).
 even(suc(suc(T))) :- even(T).
 
 odd(suc(0)).
 odd(suc(suc(T))) :- odd(T).
+
+w(0) :- write("kore ha 0").
+w(1) :- write("1 jan").
+w(2) :- nl.
